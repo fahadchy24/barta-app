26e60f2e01b31b5301ffef470b38ec5170314066
fahadchy24 <fahadchy24@gmail.com>
2024-09-13 05:10:09 +0600

    Added: name field from migration and splitted name functions on register

--- app/Http/Controllers/Auth/RegistrationController.php
+++ app/Http/Controllers/Auth/RegistrationController.php
@@ -23,18 +23,21 @@ class RegistrationController extends Controller
 
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
+        $validated = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:255', 'unique:users,username'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', Rules\Password::defaults()],
         ]);
 
+        $fullName = array_pad(explode(' ', trim($validated['name'])), 2, null);
+
         DB::table('users')->insert([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'first_name' => trim(str_replace($fullName[(count($fullName) - 1)], '', $validated['name'])),
+            'last_name' => $fullName[(count($fullName) - 1)],
+            'username' => $validated['username'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
         ]);
 
         return redirect(route('login', absolute: false))->with('success', 'Registration Successful. Please login to your account.');
